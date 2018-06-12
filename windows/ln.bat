1203427cb6cc1d2407447fa60b1bfa8b1bd4d49f
drac <extreme.mobiler@gmail.com>
2018-06-13 01:01:41 +0900

    デグレ修正

--- windows/ln.bat
+++ windows/ln.bat
@@ -1,22 +1,8 @@
-set TOOLS_DIR=C:\devel\tools\
-set CONF_DIR=C:\devel\conf\
+set TOOLS_DIR=C:\tools\
+set CONF_DIR=C:\conf\
 
-set VIMP_DIR=C:\devel\tools\
-set SUBLIME_DIR=%TOOLS_DIR%SublimeText\
 set ASR_DIR=%TOOLS_DIR%Asr\
-set KITTY_DIR=%TOOLS_DIR%kitty\
-
-cd %VIMP_DIR%
-del _vimperatorrc
-mklink _vimperatorrc %CONF_DIR%vimperator\.vimperatorrc
-if not exist %VIMP_DIR%vimperator mkdir %VIMP_DIR%vimperator
-cd %VIMP_DIR%vimperator
-rmdir /s /q plugin
-mklink /d plugin %CONF_DIR%vimperator\plugin
-
-cd %SUBLIME_DIR%Data\Packages
-rmdir /s /q User
-mklink /d User %CONF_DIR%sublime
+set CLIBOR_DIR=%TOOLS_DIR%Clibor\
 
 cd %ASR_DIR%Ubar\%Username%
 del CommandMode.key
@@ -25,13 +11,9 @@ rem not typo
 del SerchMode.key
 mklink SerchMode.key %CONF_DIR%windows\asr\SearchMode.key
 
-cd %KITTY_DIR%
-del kitty.ini
-mklink kitty.ini %CONF_DIR%windows\kitty\kitty.ini
-if not exist %KITTY_DIR%Sessions mkdir %KITTY_DIR%Sessions
-cd %KITTY_DIR%Sessions
-del Default
-mklink Default %CONF_DIR%windows\kitty\Default
+cd %CLIBOR_DIR%
+del clibor.xml
+mklink clibor.xml %CONF_DIR%windows\clibor\clibor.xml
 
 cmd /k
 exit
